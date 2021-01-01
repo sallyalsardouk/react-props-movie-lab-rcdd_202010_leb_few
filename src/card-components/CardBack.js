08507c50d50ebf2d4a3e509cfffa3fdccbbb691b
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-01 18:27:33 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,11 +12,13 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
-    if (this.props.IMDBRating !== null) {
-			return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
-		}
-		return <h4>'No Rating Found'</h4>;
+    if (this.props.IMDBRating === null){
+      return <h4>No Rating Found</h4>
+    }
+      return <img src={this.props.IMDBRating} />
   }
+
+  
 
   render() {
     return (
